2f5e7fa2b0d12f5f1a9b59009fe7ad6df73c9747
moonhi <moonhi@kbfg.com>
2023-02-24 17:48:37 +0900

    테이블 생성

--- app/src/main/java/com/e/moontto/MainActivity.kt
+++ app/src/main/java/com/e/moontto/MainActivity.kt
@@ -14,8 +14,15 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.android.volley.AuthFailureError
 import com.android.volley.Request
 import com.android.volley.RequestQueue
@@ -50,47 +57,100 @@ class MainActivity : AppCompatActivity() {
     fun MoonttoMain(
     ) {
         TitleLayout()
+        MoonttoTable()
     }
 
     @Composable
     fun TitleLayout(
     ) {
-        val lotto = mainViewModel.lottoNumbers.observeAsState(arrayListOf()).value
+        val lotto = mainViewModel.lottoMap.observeAsState(emptyMap()).value.run {
+            toList().sortedWith(compareBy { it.first }).map { it.second }
+        }
         val coroutineScope = rememberCoroutineScope()
+
         val listState = rememberLazyListState().apply {
             coroutineScope.launch {
                 if (lotto.isNotEmpty()) scrollToItem(lotto.size - 1)
             }
         }
-        Log.d("ㅇㅇㅇ", "${lotto.firstOrNull()}")
 
-        LazyRow(
-            state = listState,
-        ) {
-            itemsIndexed(items = lotto.reversed()) { round, list ->
-                Log.d("ㅇㅇㅇ", "itemsIndexed")
-                NumbersOfRound(round, list)
+        BoxWithConstraints(modifier = Modifier.fillMaxWidth()) {
+            LazyRow(
+                state = listState,
+            ) {
+                itemsIndexed(items = lotto) { round, list ->
+                    NumbersOfRound(maxWidth, round, list)
+                }
             }
         }
     }
 
     @Composable
     fun NumbersOfRound(
+        roundWidth: Dp,
         round: Int,
         numbers: List<Int>
     ) {
         Column(
-            modifier = Modifier.width(IntrinsicSize.Max)
+            modifier = Modifier.width(roundWidth).height(80.dp)
         ) {
-            Log.d("ㅇㅇㅇ", "Column")
-            Row {
-                Text(text = "${round + 1}")
-                Text(text = "회 로또 당첨번호")
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                Text(text = "${round + 1}", fontWeight = FontWeight.Bold)
+                Text(text = "회 로또 당첨번호", fontWeight = FontWeight.Bold)
             }
-            Row {
+            Row(modifier = Modifier.fillMaxSize(), horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically) {
                 repeat(7) { idx ->
-                    Text(text = "${numbers[idx]} / ")
+                    Text(text = "${numbers[idx]} / ", fontSize = 20.sp)
                 }
+            }
+        }
+    }
+
+    @Composable
+    fun MoonttoTable(
+
+    ) {
+        val 최근회차 = mainViewModel.lottoMap.value?.size ?: 1
+        val moonttoList = mainViewModel.moonttoNumbers.observeAsState(arrayListOf()).value
+            .sortedWith(
+                compareBy<MainViewModel.Companion.MoonttoNumber> { it.count }
+                .thenByDescending { 최근회차 - (it.lastRound ?: 1) }
+            )
+
+        TableRow(listOf(Pair("등수", 1f), Pair("번호", 1f), Pair("횟수", 1f), Pair("마지막 등장", 1.5f), Pair("확률", 1f)))
+
+        if (moonttoList.size > 1) {
+            LazyColumn(modifier = Modifier.fillMaxWidth()) {
+
+                itemsIndexed(items = moonttoList.subList(1, moonttoList.size)) { i, _ ->
+                    TableRow(
+                        listOf(
+                            Pair("${i + 1}", 1f),
+                            Pair("${moonttoList[i+1].number}", 1f),
+                            Pair("${moonttoList[i+1].count}", 1f),
+                            Pair("${moonttoList[i+1].lastRound}회 (${최근회차 - (moonttoList[i+1].lastRound ?: 1)})", 1.5f),
+                            Pair("${moonttoList[i+1].probability}%", 1f)
+                        )
+                    )
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun TableRow(
+        value: List<Pair<String, Float>>
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth().height(20.dp),
+//            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            repeat(value.size) { i ->
+                Text(
+                    modifier = Modifier.weight(value[i].second),
+                    style = TextStyle(textAlign = TextAlign.Center),
+                    text = value[i].first
+                )
             }
         }
     }
--- app/src/main/java/com/e/moontto/MainViewModel.kt
+++ app/src/main/java/com/e/moontto/MainViewModel.kt
@@ -19,29 +19,45 @@ import kotlin.coroutines.coroutineContext
 
 class MainViewModel(val context: Application): AndroidViewModel(context) {
 
-    val lottoNumbers: MutableLiveData<ArrayList<List<Int>>> by lazy {
-        getAllNumbers()
-    }
-    private val utils = Utils(context)
-    private val requestQueue = Volley.newRequestQueue(context)
-    private val scope = CoroutineScope(Dispatchers.Main)
+//    val lottoNumbers: MutableLiveData<ArrayList<List<Int>>> by lazy {
+//        getAllNumbers()
+//    }
 
-    private fun getAllNumbers(): MutableLiveData<ArrayList<List<Int>>> {
-        Log.d("MainViewModel", "getAllNumbers")
-        val tmpList = arrayListOf<List<Int>>()
+    val lottoMap: MutableLiveData<HashMap<Int, List<Int>>> by lazy {
+        val tmpMap = HashMap<Int, List<Int>>()
         var round = 1
         while (true) {
             val numbers = utils.getNumbersOf(round)
             if (numbers.isEmpty()) break
             else {
-                tmpList.add(numbers)
+                tmpMap[round] = numbers
                 round ++
             }
         }
-        return MutableLiveData<ArrayList<List<Int>>>().apply {
-            value = tmpList
-        }
+        MutableLiveData<HashMap<Int, List<Int>>>().apply { value = tmpMap }
     }
+
+    var moonttoNumbers: MutableLiveData<List<MoonttoNumber>> = MutableLiveData<List<MoonttoNumber>>()
+    private val utils = Utils(context)
+    private val requestQueue = Volley.newRequestQueue(context)
+    private val scope = CoroutineScope(Dispatchers.Main)
+
+//    private fun getAllNumbers(): MutableLiveData<ArrayList<List<Int>>> {
+//        Log.d("MainViewModel", "getAllNumbers")
+//        val tmpList = arrayListOf<List<Int>>()
+//        var round = 1
+//        while (true) {
+//            val numbers = utils.getNumbersOf(round)
+//            if (numbers.isEmpty()) break
+//            else {
+//                tmpList.add(numbers)
+//                round ++
+//            }
+//        }
+//        return MutableLiveData<ArrayList<List<Int>>>().apply {
+//            value = tmpList
+//        }
+//    }
 
     fun updateNumbers() {
         Log.d("MainViewModel", "updateNumbers")
@@ -50,12 +66,47 @@ class MainViewModel(val context: Application): AndroidViewModel(context) {
         }
     }
 
+//    fun requestLotto(round: Int) {
+//        Log.d("MainViewModel", "requestLotto")
+//        if (lottoNumbers.value != null && lottoNumbers.value!!.size > round && lottoNumbers.value!![round - 1].isNotEmpty()) {
+//            scope.launch { requestLotto(round + 1) }
+//        } else if (utils.getNumbersOf(round).isNotEmpty()) {
+//            lottoNumbers.value?.add(round - 1, utils.getNumbersOf(round))
+//            scope.launch { requestLotto(round + 1) }
+//        } else {
+//            val url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=$round"
+//
+//            val request = object : StringRequest(
+//                url,
+//                Response.Listener { response ->
+//                    if (response == null || response.contains("fail")) {
+//                        getMoonttoNumbers()
+//                        return@Listener
+//                    }
+//                    utils.setNumbersOf(round, response)
+//                    lottoNumbers.value?.add(round - 1, utils.getNumbersOf(round))
+//                    scope.launch { requestLotto(round + 1) }
+//                },
+//                Response.ErrorListener {
+//                    Log.d("error", "$it")
+//                    getMoonttoNumbers()
+//                }
+//            ) {
+//                override fun getParams(): MutableMap<String, String>? {
+//                    return super.getParams()
+//                }
+//            }
+//            request.setShouldCache(false)
+//            requestQueue.add(request)
+//        }
+//    }
+
     fun requestLotto(round: Int) {
         Log.d("MainViewModel", "requestLotto")
-        if (lottoNumbers.value != null && lottoNumbers.value!!.size > round && lottoNumbers.value!![round - 1].isNotEmpty()) {
+        if (lottoMap.value != null && lottoMap.value!!.size > round && lottoMap.value!![round - 1]?.isNotEmpty() == true) {
             scope.launch { requestLotto(round + 1) }
         } else if (utils.getNumbersOf(round).isNotEmpty()) {
-            lottoNumbers.value?.add(round - 1, utils.getNumbersOf(round))
+            lottoMap.value?.put(round, utils.getNumbersOf(round))
             scope.launch { requestLotto(round + 1) }
         } else {
             val url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=$round"
@@ -63,13 +114,17 @@ class MainViewModel(val context: Application): AndroidViewModel(context) {
             val request = object : StringRequest(
                 url,
                 Response.Listener { response ->
-                    if (response == null || response.contains("fail")) return@Listener
+                    if (response == null || response.contains("fail")) {
+                        getMoonttoNumbers()
+                        return@Listener
+                    }
                     utils.setNumbersOf(round, response)
-                    lottoNumbers.value?.add(round - 1, utils.getNumbersOf(round))
+                    lottoMap.value?.put(round, utils.getNumbersOf(round))
                     scope.launch { requestLotto(round + 1) }
                 },
                 Response.ErrorListener {
                     Log.d("error", "$it")
+                    getMoonttoNumbers()
                 }
             ) {
                 override fun getParams(): MutableMap<String, String>? {
@@ -79,5 +134,29 @@ class MainViewModel(val context: Application): AndroidViewModel(context) {
             request.setShouldCache(false)
             requestQueue.add(request)
         }
+    }
+
+    fun getMoonttoNumbers() {
+        val moontto = List(46) { MoonttoNumber(it) }
+        val lottoMap = lottoMap.value ?: emptyMap()
+
+        repeat (lottoMap.size) { i ->
+            lottoMap[i+1]?.forEach { number ->
+                moontto[number].apply {
+                    count ++
+                    lastRound = i + 1
+                }
+            }
+        }
+        moonttoNumbers.value = moontto
+    }
+
+    companion object {
+        data class MoonttoNumber (
+            val number: Int,
+            var count: Int = 0,
+            var lastRound: Int? = null,
+            var probability: Double = 0.0
+        )
     }
 }
